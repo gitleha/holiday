ddf34b5ac250813482e0f7e2372c46b5f5395d1e
Miroufff <sylvain.davenel@laposte.net>
2016-05-04 15:27:53 +0200

    edit build

--- src/Aqualeha/AppBundle/AqualehaAppBundle.php
+++ src/Aqualeha/AppBundle/AqualehaAppBundle.php
@@ -4,6 +4,11 @@ namespace Aqualeha\AppBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class AqualehaAppBundle
+ *
+ * @package Aqualeha\AppBundle
+ */
 class AqualehaAppBundle extends Bundle
 {
 }
--- src/Aqualeha/TwigBundle/AqualehaTwigBundle.php
+++ src/Aqualeha/TwigBundle/AqualehaTwigBundle.php
@@ -4,6 +4,11 @@ namespace Aqualeha\TwigBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class AqualehaTwigBundle
+ *
+ * @package Aqualeha\TwigBundle
+ */
 class AqualehaTwigBundle extends Bundle
 {
 
