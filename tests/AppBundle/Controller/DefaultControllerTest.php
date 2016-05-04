5ef86b1d1c6f7c462fa0d4e15f24dcc8122a1ad5
Miroufff <sylvain.davenel@laposte.net>
2016-05-04 14:58:40 +0200

    fix test index

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
+    /**
+     * @group index
+     */
     public function testIndex()
     {
         $client = static::createClient();
@@ -13,6 +16,5 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
 }
