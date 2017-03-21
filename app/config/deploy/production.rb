a88a45e46badc3ebe54dba7329f8d26403730e79
Sylvain <smaignan.web@gmail.com>
2017-03-21 11:54:59 +0100

    Conf CAP

--- app/config/deploy/production.rb
+++ app/config/deploy/production.rb
@@ -3,5 +3,7 @@ set :branch, "master"
 set :application, "holidays"
 set :deploy_to,   "/var/www/html/holidays"
 
-server '172.16.0.25', :app, :web
+server '172.16.0.62', :app, :web, :primary => true
+server '172.16.0.72', :app, :web
+
 ssh_options[:port] = "22000"
