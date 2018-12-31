a2bcd4a420315c2f34b1ac1af398a61757178934
Thibaud Hardy <thardy@aqualeha.fr>
2018-12-31 09:05:24 +0100

    suppression des fichier de conf sans .dist

--- app/config/deploy/production.rb
+++
@@ -1,9 +0,0 @@
-set :stage,  :production
-set :branch, "master"
-set :application, "holidays"
-set :deploy_to,   "/var/www/html/holidays"
-
-server '172.16.0.62', :app, :web, :primary => true
-server '172.16.0.72', :app, :web
-
-ssh_options[:port] = "22000"
