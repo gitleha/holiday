15c997b0734d521ea561a907d5ec14f3560fd5bd
corentin <conicole@protonmail.com>
2019-10-28 12:23:28 +0100

    Readd Apache for foreground process in docker

--- docker/start.sh
+++ docker/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-echo "Start holiday docker"
-#rm -f /run/apache2/apache2.pid
-#rm -f /run/apache2/httpd.pid
-#
-#echo "Starting apache..."
-#httpd -D FOREGROUND
+echo "Clearing any old processes..."
+rm -f /run/apache2/apache2.pid
+rm -f /run/apache2/httpd.pid
+
+echo "Starting apache..."
+httpd -D FOREGROUND
