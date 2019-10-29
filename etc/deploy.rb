09384591990096c0ff3c5c7da4be0edb74d52934
corentin <conicole@protonmail.com>
2019-10-29 10:21:41 +0100

    Add var sessions

--- etc/deploy.rb
+++ etc/deploy.rb
@@ -4,10 +4,10 @@ lock "~> 3.11.0"
 set :application, "holiday"
 set :repo_url, "git@github.com:gitleha/holiday.git"
 set :deploy_to, "/var/www/html/holiday"
-set :file_permissions_paths, ["var/logs", "var/cache"]
+set :file_permissions_paths, ["var/logs", "var/cache", "var/sessions"]
 set :file_permissions_users, ["apache"]
 
 before "deploy:updated", "deploy:set_permissions:acl"
 set :pty, true
 append :linked_files, "app/config/parameters.yml"
-append :linked_dirs, "var/logs", "var/cache"
+append :linked_dirs, "var/logs", "var/cache", "var/sessions"
