69f7c579784adcb134792f8cf6d7e118400540c4
corentin <conicole@protonmail.com>
2019-10-29 09:43:29 +0100

    Fix log path on cap

--- etc/deploy.rb
+++ etc/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.11.0"
 set :application, "holiday"
 set :repo_url, "git@github.com:gitleha/holiday.git"
 set :deploy_to, "/var/www/html/holiday"
-set :file_permissions_paths, ["var/log", "var/cache"]
+set :file_permissions_paths, ["var/logs", "var/cache"]
 set :file_permissions_users, ["apache"]
 
 before "deploy:updated", "deploy:set_permissions:acl"
