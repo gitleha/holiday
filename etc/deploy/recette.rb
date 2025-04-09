1f1ea39e084d9b25499cdbe50adae3584c47da96
Sylvain <smaignan.web@gmail.com>
2025-04-09 12:19:07 +0200

    Jenkins

--- etc/deploy/recette.rb
+++ etc/deploy/recette.rb
@@ -3,8 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server 'otools01:22', user: 'root'
-server 'otools02:22', user: 'root'
+server 'rotools:22', user: 'root'
 
 set :stage, :recette
 set :branch, "dev"
