dfdbbe6d7c8246cab9c300b041e91b6286a46fb0
Sylvain <smaignan.web@gmail.com>
2025-04-17 17:44:52 +0200

    Jenkins

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -3,7 +3,10 @@ lock "3.10.2"
 
 set :application, "holiday"
 set :repo_url, "git@github.com:gitleha/holiday.git"
-set :deploy_to, "/var/www/html/holiday"
+set :symfony_directory_structure, 3
+set :use_composer, true
+set :keep_releases, 3
+
 set :file_permissions_paths, ["var/logs", "var/cache", "var/sessions"]
 set :file_permissions_users, ["www-data"]
 
